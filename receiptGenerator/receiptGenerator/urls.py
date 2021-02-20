1f25d1cc41aa89bee0db36b2e74898fdef7d2a29
Catarina <c.alexandracorreia@ua.pt>
2021-02-20 23:29:34 +0000

    Add form

--- receiptGenerator/receiptGenerator/urls.py
+++ receiptGenerator/receiptGenerator/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from django.conf.urls import url
+
+from app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
 ]
