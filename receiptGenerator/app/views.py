3b5856ec4a3fbd27f672bd881c95672bd4cdc634
Catarina <c.alexandracorreia@ua.pt>
2021-02-21 00:00:58 +0000

    add form

--- receiptGenerator/app/views.py
+++ receiptGenerator/app/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from .forms import ReceiptForm
+
 
 def index(request):
-    return render(request, 'form.html')
+    submitbutton= request.POST.get("submit")
+
+    firstname=''
+    lastname=''
+
+    form= ReceiptForm(request.POST or None)
+    if form.is_valid():
+        firstname= form.cleaned_data.get("firstname")
+        lastname= form.cleaned_data.get("lastname")
+
+
+    context= {'form': form, 'firstname': firstname, 'lastname':lastname,
+              'submitbutton': submitbutton}
+        
+    return render(request, 'form.html', context)
