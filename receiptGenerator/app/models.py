026909cbf9af86e15196bf70ef605f44e3832357
Catarina <c.alexandracorreia@ua.pt>
2021-03-09 16:03:27 +0000

    Update docker-compose

--- receiptGenerator/app/models.py
+++ receiptGenerator/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+class Receipt_Block(models.Model):
+    id_receipt = models.CharField(unique=True)
+    timestamp_stored = models.DateTimeField(auto_now_add = True)
+    current_hash = models.CharField()
+    json_receipt = JSONField()
+    #timestamp_receipt = models.CharField()
+    #timestamp_stored = models.DateTimeField(auto_now_add = True) # to know when the receipt was stored on the database
+    #timestamp_updated = models.DateTimeField(auto_now = True)
--- receiptGenerator/receiptGenerator/settings.py
+++ receiptGenerator/receiptGenerator/settings.py
@@ -77,9 +77,16 @@ WSGI_APPLICATION = 'receiptGenerator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': ‘<db_name>’,
+        'USER': '<db_username>',
+        'PASSWORD': '<password>',
+        'HOST': '<db_hostname_or_ip>',
+        'PORT': '<db_port>',
     }
+
 }
 
 
