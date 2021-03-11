c008db65d3f3c6ca6da0d5e7aedf8a1623fb3e84
Catarina <c.alexandracorreia@ua.pt>
2021-03-11 12:08:01 +0000

    Add app on installed apps and fix error on models

--- receiptGenerator/app/models.py
+++ receiptGenerator/app/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.contrib.postgres.fields import JSONField
 
 class Receipt_Block(models.Model):
-    id_receipt = models.CharField(unique=True)
+    id_receipt = models.CharField(unique=True, max_length = 100)
     #current_hash = models.CharField()
     json_receipt = JSONField()
-    timestamp_receipt = models.CharField()
+    timestamp_receipt = models.CharField(max_length = 100)
     timestamp_stored = models.DateTimeField(auto_now_add = True) # to know when the receipt was stored on the database
     timestamp_updated = models.DateTimeField(auto_now = True)
