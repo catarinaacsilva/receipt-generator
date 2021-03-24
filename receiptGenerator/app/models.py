cffe12939c96fc7e900a34830a826d46ce74aec6
Catarina <c.alexandracorreia@ua.pt>
2021-03-24 14:28:44 +0000

    Update blockchain

--- receiptGenerator/app/models.py
+++ receiptGenerator/app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import JSONField
 
+'''
 class Receipt_Block(models.Model):
     id_receipt = models.CharField(unique=True, max_length = 100)
     #current_hash = models.CharField()
@@ -8,3 +9,10 @@ class Receipt_Block(models.Model):
     timestamp_receipt = models.CharField(max_length = 100)
     timestamp_stored = models.DateTimeField(auto_now_add = True) # to know when the receipt was stored on the database
     #timestamp_updated = models.DateTimeField(auto_now = True)
+'''
+
+class Chain(models.Model):
+    id_chain = models.AutoField(primary_key=True)
+    json_block = JSONField()
+    json_receipt = JSONField()
+    timestamp = models.DateTimeField(auto_now_add = True)
--- receiptGenerator/app/views.py
+++ receiptGenerator/app/views.py
@@ -16,12 +16,15 @@ from rest_framework.decorators import api_view
 
 from django.conf import settings
 from .models import Receipt_Block
+from .blockchain import Blockchain
 
 logger = logging.getLogger(__name__)
 
 #cache
 
 key = None
+
+blockchain = Blockchain()
 
 '''
     Initial page just to init the demo
@@ -143,99 +146,9 @@ def receipt_valid(request):
     return JsonResponse(response)
 
 
-''' ########################################################################
-                                BlockChain API
-########################################################################'''
-
-class Blockchain:
-
-    def __init__(self):
-        self.chain = []
-        self.transactions = [] #New
-        self.create_block(nonce = 1, previous_hash = '0')
-        self.nodes = set() #New
-
-    def create_block(self, nonce, previous_hash, data):
-        block = {'index': len(self.chain) + 1,
-                 'timestamp': datetime.timestamp(now),
-                 'nonce': nonce,
-                 'previous_hash': previous_hash,
-                 'data': data
-                 #'transactions': self.transactions #New
-                }
-        self.transactions = [] #New
-        self.chain.append(block)
-        return block
-
-    def get_last_block(self):
-        return self.chain[-1]
-
-    def proof_of_work(self, previous_nonce):
-        new_nonce = 1
-        check_nonce = False
-        while check_nonce is False:
-            hash_operation = hashlib.sha256(str(new_nonce**2 - previous_nonce**2).encode()).hexdigest()
-            if hash_operation[:4] == '0000':
-                check_nonce = True
-            else:
-                new_nonce += 1
-        return new_nonce
-
-    def hash(self, block):
-        encoded_block = json.dumps(block, sort_keys = True).encode()
-        return hashlib.sha256(encoded_block).hexdigest()
-
-    def is_chain_valid(self, chain):
-        previous_block = chain[0]
-        block_index = 1
-        while block_index < len(chain):
-            block = chain[block_index]
-            if block['previous_hash'] != self.hash(previous_block):
-                return False
-            previous_nonce = previous_block['nonce']
-            nonce = block['nonce']
-            hash_operation = hashlib.sha256(str(nonce**2 - previous_nonce**2).encode()).hexdigest()
-            if hash_operation[:4] != '0000':
-                return False
-            previous_block = block
-            block_index += 1
-        return True
-
-    '''
-    def add_transaction(self, sender, receiver, amount, time): #New
-        self.transactions.append({'sender': sender,
-                                  'receiver': receiver,
-                                  'amount': amount,
-                                  'time': str(datetime.datetime.now())})
-        previous_block = self.get_last_block()
-        return previous_block['index'] + 1
-    '''
-
-    def add_node(self, address): 
-        parsed_url = urlparse(address)
-        self.nodes.add(parsed_url.netloc)
-
-
-    def replace_chain(self):
-        network = self.nodes
-        longest_chain = None
-        max_length = len(self.chain)
-        for node in network:
-            response = requests.get(f'http://{node}/get_chain')
-            if response.status_code == 200:
-                length = response.json()['length']
-                chain = response.json()['chain']
-                if length > max_length and self.is_chain_valid(chain):
-                    max_length = length
-                    longest_chain = chain
-        if longest_chain:
-            self.chain = longest_chain
-            return True
-        return False
-
 
 # Creating our Blockchain
-blockchain = Blockchain()
+
 # Creating an address for the node running our server
 #node_address = str(uuid4()).replace('-', '') #New
 #root_node = 'e36f0158f0aed45b3bc755dc52ed4560d' #New
