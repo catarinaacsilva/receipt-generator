e96baf3a9d6a95205e6ed0a76b44a89dfdf09d84
Catarina <c.alexandracorreia@ua.pt>
2021-03-24 21:56:10 +0000

    Add store receipt

--- receiptGenerator/app/views.py
+++ receiptGenerator/app/views.py
@@ -14,9 +14,14 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-from django.conf import settings
+
 from .models import Receipt_Block
+from .models import Chain
+
+from django.conf import settings
 from .blockchain import Blockchain
+
+
 
 logger = logging.getLogger(__name__)
 
@@ -120,117 +125,19 @@ def reply_receipt(request):
     json_receipt = json.loads(request.body)
     logger.info(json_receipt)
 
+    #TODO: Check receipt
+
     try:
-        url = settings.DATA_RETENTION_RECEIPT
-        receipt = {'id_receipt':json_receipt['Receipt ID'], 'receipt_timestamp':json_receipt['Receipt Timestamp']}
-        x = requests.post(url, data=receipt)
-        block = mine_block(json_receipt)
-        Receipt_Block.objects.create(json_receipt['Receipt ID'], block, json_receipt['Receipt Timestamp'])
+        #url = settings.DATA_RETENTION_RECEIPT
+        #receipt = {'id_receipt':json_receipt['Receipt ID'], 'receipt_timestamp':json_receipt['Receipt Timestamp']}
+        #x = requests.post(url, data=json_receipt)
+        with transaction.atomic():
+            block = mine_block(json_receipt)
+            Chain.objects.create(json_block=block, json_receipt=json_receipt)
         #Receipt_Block.objects.create(json_receipt['Receipt ID'], json_receipt, json_receipt['Receipt Timestamp'])
-    except:
+    except Exception as e:
+        handle_exception()
+        print(e)
         return Response('Cannot create the receipt record', status=status.HTTP_400_BAD_REQUEST)
     
     return Response(status=status.HTTP_201_CREATED)
-
-'''
-    Chech if the receipt chain is valid
-'''
-@csrf_exempt
-@api_view(('GET',))
-def receipt_valid(request):
-    is_valid = blockchain.is_chain_valid(blockchain.chain)
-    if is_valid:
-        response = {'The chain is valid', status=status.HTTP_200_OK}
-    else:
-        response = {'The chain is not valid', status=status.HTTP_400_BAD_REQUEST}
-    return JsonResponse(response)
-
-
-
-# Creating our Blockchain
-
-# Creating an address for the node running our server
-#node_address = str(uuid4()).replace('-', '') #New
-#root_node = 'e36f0158f0aed45b3bc755dc52ed4560d' #New
-
-# Mining a new block
-#@csrf_exempt
-#@api_view(('GET',))
-#def mine_block(request):
-def mine_block(data):
-    if not Chain.objects.exists():
-        block = blockchain.create_block(nonce = 1, previous_hash = '0', data=data)
-    else:
-        previous_block = blockchain.get_last_block()
-        previous_nonce = previous_block['nonce']
-        nonce = blockchain.proof_of_work(previous_nonce)
-        previous_hash = blockchain.hash(previous_block)
-        #blockchain.add_transaction(sender = root_node, receiver = node_address, amount = 1.15, time=str(datetime.datetime.now()))
-        block = blockchain.create_block(nonce, previous_hash, data)
-    response = {'message': 'Congratulations, you just mined a block!',
-                    'index': block['index'],
-                    'timestamp': block['timestamp'],
-                    'nonce': block['nonce'],
-                    'previous_hash': block['previous_hash'],
-                    'data': data
-                    #'transactions': block['transactions']}
-    return JsonResponse(response)
-
-# Getting the full Blockchain
-def get_chain(request):
-    if request.method == 'GET':
-        response = {'chain': blockchain.chain,
-                    'length': len(blockchain.chain)}
-    return JsonResponse(response)
-
-# Checking if the Blockchain is valid
-'''
-def is_valid(request):
-    if request.method == 'GET':
-        is_valid = blockchain.is_chain_valid(blockchain.chain)
-        if is_valid:
-            response = {'message': 'All good. The Blockchain is valid.'}
-        else:
-            response = {'message': 'The Blockchain is not valid.'}
-    return JsonResponse(response)
-'''
-
-'''
-# Adding a new transaction to the Blockchain
-@csrf_exempt
-def add_transaction(request): #New
-    if request.method == 'POST':
-        received_json = json.loads(request.body)
-        transaction_keys = ['sender', 'receiver', 'amount','time']
-        if not all(key in received_json for key in transaction_keys):
-            return 'Some elements of the transaction are missing', HttpResponse(status=400)
-        index = blockchain.add_transaction(received_json['sender'], received_json['receiver'], received_json['amount'],received_json['time'])
-        response = {'message': f'This transaction will be added to Block {index}'}
-    return JsonResponse(response)
-'''
-
-# Connecting new nodes
-@csrf_exempt
-def connect_node(request):
-    if request.method == 'POST':
-        received_json = json.loads(request.body)
-        nodes = received_json.get('nodes')
-        if nodes is None:
-            return "No node", HttpResponse(status=400)
-        for node in nodes:
-            blockchain.add_node(node)
-        response = {'message': 'All the nodes are now connected. The Sudocoin Blockchain now contains the following nodes:',
-                    'total_nodes': list(blockchain.nodes)}
-    return JsonResponse(response)
-
-# Replacing the chain by the longest chain if needed
-def replace_chain(request): 
-    if request.method == 'GET':
-        is_chain_replaced = blockchain.replace_chain()
-        if is_chain_replaced:
-            response = {'message': 'The nodes had different chains so the chain was replaced by the longest one.',
-                        'new_chain': blockchain.chain}
-        else:
-            response = {'message': 'All good. The chain is the largest one.',
-                        'actual_chain': blockchain.chain}
-    return JsonResponse(response)
